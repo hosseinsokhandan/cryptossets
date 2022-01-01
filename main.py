fa9519492fda271ab0a79b129243dd81a7c6d43e
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-01 18:25:43 +0330

    main account support tempaltes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from requests.sessions import Request
+from starlette.responses import HTMLResponse
 
 from exchanges import kucoin
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/kucoin')
-def kucoin_assets():
+
+@app.get('/kucoin', response_class=HTMLResponse)
+async def kucoin_assets(request: Request):
+    balance = kucoin.get_balance("")
     orders = kucoin.get_orders("")
-    print(len(orders))
-    return orders
+    context = {"request": request, "id": id}
+    return templates.TemplateResponse("item.html", context)
