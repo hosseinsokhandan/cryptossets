4cf539d4c94e91de433d042ae274e9d376f6f5be
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-02 16:45:04 +0330

    / and /kucoin finalized for kucoin.py at main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from requests.sessions import Request
+from starlette.requests import Request
 from starlette.responses import HTMLResponse
 
 from exchanges import kucoin
@@ -10,10 +10,16 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
+@app.get('/', response_class=HTMLResponse)
+async def homepage(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("homepage.html", context)
+
+
 @app.get('/kucoin', response_class=HTMLResponse)
 async def kucoin_assets(request: Request):
-    balance = kucoin.get_balance("")
-    orders = kucoin.get_orders("")
+    balance = kucoin.get_balance()
+    orders = kucoin.get_orders()
     data = kucoin.calculate(balance, orders)
     context = {"request": request, "data": data}
     return templates.TemplateResponse("kucoin.html", context)
